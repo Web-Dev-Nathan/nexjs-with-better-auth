90e6202e249e1de356b3af812f9306d4bc19a15b
Web-dev-nathan <ntesfaye000@gamil.com>
2025-03-23 03:50:51 +0300

    Refactor email function to remove unused error handling from email sending process

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function email({to,subject,text}: {to: string, subject: string, text: string}) {
   try {
-    const { data, error } = await resend.emails.send({
+    const { data } = await resend.emails.send({
       from: process.env.Email_address as string,
       to,
       subject,
