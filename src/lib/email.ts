3fe4e1725b5af0464ae384e47493a8331cf43605
Web-dev-nathan <ntesfaye000@gamil.com>
2025-03-23 03:41:49 +0300

    Update email sender to use environment variable for email address

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function email({to,subject,text}: {to: string, subject: string, text: string}) {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'ntesfaye000@gmail.com',
+      from: process.env.Email_address as string,
       to,
       subject,
       text,
